0f6cc5876678e7e20a73560f3c3ba4934dcb23e6
ase21 <ase911@yandex.ru>
2019-12-20 13:17:11 +0300

    Timer is finished

--- app/src/main/java/com/asefactory/ase21/westmeathscoreapp/mainview/MainFragment.java
+++ app/src/main/java/com/asefactory/ase21/westmeathscoreapp/mainview/MainFragment.java
@@ -5,8 +5,10 @@ import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -21,13 +23,21 @@ import moxy.MvpAppCompatFragment;
 
 public class MainFragment extends MvpAppCompatFragment {
 
-    @BindView(R.id.timeTextView)
-    EditText timeTextView;
-    private Unbinder unbinder;
-    private boolean chronometerFlag = false;
-
     @BindView(R.id.chronometer)
     Chronometer chronometer;
+    @BindView(R.id.timeTextView)
+    EditText timeTextView;
+    @BindView(R.id.startChronometerButton)
+    Button startChronometerButton;
+    @BindView(R.id.halfNameTextView)
+    TextView halfNameTextView;
+
+    private Unbinder unbinder;
+    private boolean chronometerStarted = false;
+    private boolean isFirstHalfIsFinished = false;
+
+    private long firstHalfTime;
+    private long secondHalfTime;
 
     @Nullable
     @Override
@@ -39,25 +49,66 @@ public class MainFragment extends MvpAppCompatFragment {
 
     private void initUI(View view) {
         unbinder = ButterKnife.bind(this, view);
-        chronometer.setOnChronometerTickListener(chronometer -> {
-            long elapsedMillis = SystemClock.elapsedRealtime() - chronometer.getBase();
-
-            long time = Long.parseLong(timeTextView.getText().toString()) * 1000 * 60;
-
-            if (elapsedMillis > time){
-                chronometer.stop();
-                chronometer.setBase(SystemClock.elapsedRealtime());
-                chronometerFlag = false;
-            }
-        });
     }
 
     @OnClick(R.id.startChronometerButton)
     void onStartChronometerClicked() {
-        if (!chronometerFlag) {
-            chronometer.setBase(SystemClock.elapsedRealtime());
-            chronometer.start();
-            chronometerFlag = true;
-        }
+        checkValidTextFields();
+        initChronometer();
+    }
+
+    private void checkValidTextFields() {
+        firstHalfTime = Long.parseLong(timeTextView.getText().toString()) * 1000 * 60;
+        secondHalfTime = Long.parseLong(timeTextView.getText().toString()) * 1000 * 60 * 2;
+    }
+
+    private void initChronometer() {
+            if(!isFirstHalfIsFinished) {
+                if (!chronometerStarted) {
+                    prepareChronometer(SystemClock.elapsedRealtime(), firstHalfTime);
+                    startChronometerButton.setText(getResources().getText(R.string.half_time));
+                    halfNameTextView.setText(getResources().getText(R.string.first_half));
+                } else {
+                    chronometer.stop();
+                    chronometer.setTextColor(getResources().getColor(android.R.color.black));
+                    startChronometerButton.setText(getResources().getText(R.string.second_half));
+                    isFirstHalfIsFinished = true;
+                    chronometerStarted = false;
+                }
+            } else {
+                if (!chronometerStarted) {
+                    prepareChronometer(SystemClock.elapsedRealtime() - firstHalfTime, secondHalfTime);
+                    startChronometerButton.setText(getResources().getText(R.string.full_time));
+                    halfNameTextView.setText(getResources().getText(R.string.second_half));
+                } else {
+                    chronometer.stop();
+                    chronometer.setTextColor(getResources().getColor(android.R.color.black));
+                    startChronometerButton.setText(getResources().getText(R.string.second_half));
+                    isFirstHalfIsFinished = true;
+                    chronometerStarted = false;
+                }
+            }
+
+    }
+
+    private void prepareChronometer(long l, long firstHalfTime) {
+        setChronometerTime(l);
+        setChronometerClickListiner(firstHalfTime);
+        chronometer.start();
+        chronometerStarted = true;
+    }
+
+    private void setChronometerTime(long time) {
+        chronometer.setBase(time);
+    }
+
+    private void setChronometerClickListiner(long firstHalfTime) {
+        chronometer.setOnChronometerTickListener(chronometer -> {
+            long elapsedMillis = SystemClock.elapsedRealtime() - chronometer.getBase();
+
+            if (elapsedMillis > firstHalfTime) {
+                chronometer.setTextColor(getResources().getColor(R.color.colorAccent));
+            }
+        });
     }
 }
